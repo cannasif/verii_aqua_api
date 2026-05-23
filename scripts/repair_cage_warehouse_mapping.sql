4ff9a8b1d0879b342b77071a88d40ae1ffb6dcda
Can Nasıf <can.nasif@v3rii.com>
2026-05-23 15:30:02 +0300

    Add cage warehouse mapping repair script

---
+++ scripts/repair_cage_warehouse_mapping.sql
@@ -0,0 +1,106 @@
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NULL
+BEGIN
+    CREATE TABLE [dbo].[RII_CageWarehouseMapping] (
+        [Id] bigint IDENTITY(1,1) NOT NULL,
+        [CageId] bigint NOT NULL,
+        [WarehouseId] bigint NOT NULL,
+        [IsActive] bit NOT NULL CONSTRAINT [DF_RII_CageWarehouseMapping_IsActive] DEFAULT (CONVERT(bit, 1)),
+        [Note] nvarchar(500) NULL,
+        [CreatedDate] datetime2 NOT NULL CONSTRAINT [DF_RII_CageWarehouseMapping_CreatedDate] DEFAULT (GETDATE()),
+        [UpdatedDate] datetime2 NULL,
+        [DeletedDate] datetime2 NULL,
+        [IsDeleted] bit NOT NULL,
+        [CreatedBy] bigint NULL,
+        [UpdatedBy] bigint NULL,
+        [DeletedBy] bigint NULL,
+        CONSTRAINT [PK_RII_CageWarehouseMapping] PRIMARY KEY ([Id])
+    );
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND OBJECT_ID(N'[dbo].[RII_Cage]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE [name] = N'FK_RII_CageWarehouseMapping_RII_Cage_CageId')
+BEGIN
+    ALTER TABLE [dbo].[RII_CageWarehouseMapping] WITH CHECK
+    ADD CONSTRAINT [FK_RII_CageWarehouseMapping_RII_Cage_CageId]
+    FOREIGN KEY ([CageId]) REFERENCES [dbo].[RII_Cage] ([Id]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND OBJECT_ID(N'[dbo].[RII_Warehouse]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE [name] = N'FK_RII_CageWarehouseMapping_RII_Warehouse_WarehouseId')
+BEGIN
+    ALTER TABLE [dbo].[RII_CageWarehouseMapping] WITH CHECK
+    ADD CONSTRAINT [FK_RII_CageWarehouseMapping_RII_Warehouse_WarehouseId]
+    FOREIGN KEY ([WarehouseId]) REFERENCES [dbo].[RII_Warehouse] ([Id]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND OBJECT_ID(N'[dbo].[RII_USERS]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE [name] = N'FK_RII_CageWarehouseMapping_RII_USERS_CreatedBy')
+BEGIN
+    ALTER TABLE [dbo].[RII_CageWarehouseMapping] WITH CHECK
+    ADD CONSTRAINT [FK_RII_CageWarehouseMapping_RII_USERS_CreatedBy]
+    FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[RII_USERS] ([Id]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND OBJECT_ID(N'[dbo].[RII_USERS]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE [name] = N'FK_RII_CageWarehouseMapping_RII_USERS_UpdatedBy')
+BEGIN
+    ALTER TABLE [dbo].[RII_CageWarehouseMapping] WITH CHECK
+    ADD CONSTRAINT [FK_RII_CageWarehouseMapping_RII_USERS_UpdatedBy]
+    FOREIGN KEY ([UpdatedBy]) REFERENCES [dbo].[RII_USERS] ([Id]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND OBJECT_ID(N'[dbo].[RII_USERS]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.foreign_keys WHERE [name] = N'FK_RII_CageWarehouseMapping_RII_USERS_DeletedBy')
+BEGIN
+    ALTER TABLE [dbo].[RII_CageWarehouseMapping] WITH CHECK
+    ADD CONSTRAINT [FK_RII_CageWarehouseMapping_RII_USERS_DeletedBy]
+    FOREIGN KEY ([DeletedBy]) REFERENCES [dbo].[RII_USERS] ([Id]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [name] = N'IX_RII_CageWarehouseMapping_CreatedBy' AND [object_id] = OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]'))
+BEGIN
+    CREATE INDEX [IX_RII_CageWarehouseMapping_CreatedBy] ON [dbo].[RII_CageWarehouseMapping] ([CreatedBy]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [name] = N'IX_RII_CageWarehouseMapping_UpdatedBy' AND [object_id] = OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]'))
+BEGIN
+    CREATE INDEX [IX_RII_CageWarehouseMapping_UpdatedBy] ON [dbo].[RII_CageWarehouseMapping] ([UpdatedBy]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [name] = N'IX_RII_CageWarehouseMapping_DeletedBy' AND [object_id] = OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]'))
+BEGIN
+    CREATE INDEX [IX_RII_CageWarehouseMapping_DeletedBy] ON [dbo].[RII_CageWarehouseMapping] ([DeletedBy]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [name] = N'IX_RII_CageWarehouseMapping_WarehouseId' AND [object_id] = OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]'))
+BEGIN
+    CREATE INDEX [IX_RII_CageWarehouseMapping_WarehouseId] ON [dbo].[RII_CageWarehouseMapping] ([WarehouseId]);
+END
+GO
+
+IF OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]', N'U') IS NOT NULL
+   AND NOT EXISTS (SELECT 1 FROM sys.indexes WHERE [name] = N'UX_RII_CageWarehouseMapping_Cage_Active' AND [object_id] = OBJECT_ID(N'[dbo].[RII_CageWarehouseMapping]'))
+BEGIN
+    CREATE UNIQUE INDEX [UX_RII_CageWarehouseMapping_Cage_Active]
+    ON [dbo].[RII_CageWarehouseMapping] ([CageId])
+    WHERE [IsDeleted] = 0 AND [IsActive] = 1;
+END
+GO
